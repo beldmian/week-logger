e102b975198f7e7c945239c7479a697bfe154153
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-12 17:26:55 +0000

    Deploy beldmian/week-logger to github.com/beldmian/week-logger.git:gh-pages

---
+++ precache-manifest.72788ce2c56471be0a245431c27e70af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705f5af8bc2fc6853a12",
+    "url": "/week-logger/css/app.5a3774ca.css"
+  },
+  {
+    "revision": "3354decb0fc320d5b300d798d55c390f",
+    "url": "/week-logger/index.html"
+  },
+  {
+    "revision": "705f5af8bc2fc6853a12",
+    "url": "/week-logger/js/app.798c73c6.js"
+  },
+  {
+    "revision": "1ab04a712b0c989ebcbb",
+    "url": "/week-logger/js/chunk-vendors.3e1e3367.js"
+  },
+  {
+    "revision": "45e7149a917b2a8fc676c2b146984818",
+    "url": "/week-logger/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/week-logger/robots.txt"
+  }
+]);
